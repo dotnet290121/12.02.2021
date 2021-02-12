92afdd7df11c0a384464e75323729a7f15bdff73
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-02-12 13:47:33 +0200

    Update sql_training.sql

--- sql_training.sql
+++ sql_training.sql
@@ -30,11 +30,37 @@ select * from
     where c.row_num between 1 AND 5;
 
 -- find the class where the 2nd max grade is the lowest
+select * from -- select grade from ...
+(select *,
+       row_number() over (partition by class_id order by grade desc) row_num
+       from grades ) c
+    where c.row_num = 2
+order by c.grade
+limit 1;
+
 -- find how many grades are duplicated
+select * from -- select grade from ...
+(select *,
+       row_number() over (partition by grade order by grade desc) row_num
+       from grades ) c
+    where c.row_num = 2;
+
 -- create players table (see on the right side of the screen)
+insert into players(name) values ('danny');
+insert into players(name) values ('moshe');
+insert into players(name) values ('danny');
+insert into players(name) values ('danny');
+insert into players(name) values ('pazit');
+select name from
+(select *,
+       row_number() over (partition by name order by name) row_num
+       from players ) c
+    where c.row_num   = 2;
 -- insert few players with same name (also with different names)
 -- find how many players names are duplicated
+select count(*) from
+(select *,
+       row_number() over (partition by name order by name) row_num
+       from players ) c
+    where c.row_num   = 2;
 
-delete from grades2
-where id >= 0;
-select * from sp_populate_grades2(3, 30);
